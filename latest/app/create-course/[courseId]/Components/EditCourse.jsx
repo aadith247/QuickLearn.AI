e09e85a759171bb4cd3853b2c59907ae7c10fa99
Aadithhya Thatipally... <thatipallyaadithhya@gmail.com>
2025-08-20 07:16:46 +0530

    Update EditCourse.jsx

--- latest/app/create-course/[courseId]/Components/EditCourse.jsx
+++ latest/app/create-course/[courseId]/Components/EditCourse.jsx
@@ -6,11 +6,11 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '../../../../components/ui/input';
+} from '@/components/ui/input';
 
-import { Input } from '../../../../components/ui/input';
+import { Input } from '@/components/ui/input';
 
-import { Button } from '../../../../components/ui/button';
+import { Button } from '@/components/ui/button';
 
 function EditCourse({ initialTitle, initialDescription, open, onOpenChange, onSave, courseId }) {
   const [title, setTitle] = useState(initialTitle );
