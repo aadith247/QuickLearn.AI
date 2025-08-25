37e77d0fc86ef270d6c39a1cc2f736de7e97596c
aadith247 <thatipallyaadithhya@gmail.com>
2025-08-25 14:40:21 +0530

    new update

--- app/latest/middleware.js
+++ app/latest/middleware.js
@@ -1,14 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
 
 const isProtected = createRouteMatcher(['/dashboard(.*)']);
 
 export default clerkMiddleware((auth, req) => {
-  const host = req.headers.get('host');
-  if (host === 'quick-learn-ai-ynxs-qcxr4hth2-aadith247s-projects.vercel.app') {
-    const redirectUrl = new URL(`${req.nextUrl.pathname}${req.nextUrl.search}`, 'https://quick-learn-ai-ynxs.vercel.app');
-    return NextResponse.redirect(redirectUrl, 308);
-  }
   if (isProtected(req)) {
     auth.protect(); // ✅ Correct usage
   }
